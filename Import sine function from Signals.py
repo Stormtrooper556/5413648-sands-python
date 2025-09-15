10e7e10b2ac0114645e2cc3eafd659c48a7a372b
Stormtrooper556 <tjs.deleeuw@gmail.com>
2025-09-15 22:08:48 +0200

    Plot addition

--- Import sine function from Signals.py
+++ Import sine function from Signals.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
 from Signals import generate_sine_wave
 
 # Parameters
@@ -6,7 +8,15 @@ duration = 2           # seconds
 sample_rate = 100      # samples per second
 
 # Generate the sine wave
-wave = generate_sine_wave(frequency, duration, sample_rate)
+t, waveform = generate_sine_wave(frequency, duration, sample_rate)
 
 # Print the first 10 samples
-print(wave[:10])
+print(waveform[:10])
+
+# Plot the sine wave
+plt.plot(t, waveform)
+plt.title("5 Hz Sine Wave")
+plt.xlabel("Time (seconds)")
+plt.ylabel("Amplitude")
+plt.grid(True)
+plt.show()
