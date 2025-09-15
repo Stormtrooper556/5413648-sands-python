4166056d6d924f9fad8116a21df426b42d75644c
Stormtrooper556 <tjs.deleeuw@gmail.com>
2025-09-15 22:08:14 +0200

    Plot addition

--- Signals.py
+++ Signals.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 def generate_sine_wave(frequency, duration, sample_rate):
-	t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
-    	waveform = np.sin(2 * np.pi * frequency * t)
-    	return waveform
+    t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
+    waveform = np.sin(2 * np.pi * frequency * t)
+    return t, waveform
