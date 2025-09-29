b09f14a0de420d48b7d48f2dbd81ac26f6ed07ea
Stormtrooper556 <tjs.deleeuw@gmail.com>
2025-09-29 17:17:19 +0200

    Sawtooth + periodic pulse function add

--- Signals.py
+++ Signals.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy as sp #Or just import the signal module from scipy: from scipy import signal. And proceed with signal.square() e.g.
 
 def generate_sine_wave(frequency, duration, sample_rate):
     t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
@@ -7,3 +8,9 @@ def generate_sine_wave(frequency, duration, sample_rate):
 
 def pulse(t, amp):
     return np.where((t >= -0.5) & (t <= 0.5), amp, 0.0)
+
+def periodic_pulse(t, duty_cycle):
+    return sp.signal.square(t, duty_cycle)
+
+def sawtooth(t, width):
+    return sp.signal.sawtooth(t, width)
