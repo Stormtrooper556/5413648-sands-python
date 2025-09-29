39ba067b6043e420b5dabb7a2666dddc325d6c6b
Stormtrooper556 <tjs.deleeuw@gmail.com>
2025-09-29 16:38:03 +0200

    Pulse function addition

--- Signals.py
+++ Signals.py
@@ -4,3 +4,6 @@ def generate_sine_wave(frequency, duration, sample_rate):
     t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
     waveform = np.sin(2 * np.pi * frequency * t)
     return t, waveform
+
+def pulse(t, amp):
+    return np.where((t >= -0.5) & (t <= 0.5), amp, 0.0)
